1190773830a36484e15b498e7750c40a5527f3a0
mnieber <mnieber@9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf>
2008-09-22 11:06:44 +0000

    Fix in call to ReplaceDestinationFileIfDifferent
    git-svn-id: https://svncistib.upf.edu/repos/csnake/CSnake@178 9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf

--- csnKDevelop.py
+++ csnKDevelop.py
@@ -49,7 +49,7 @@ class PostProcessor:
                 f.write(fileListItem + "/n")
         f.close()
         
-        csnUtility.ReplaceDestinationFileIfDifferent(self.__GetFilelistFilename(_project, kdevelopProjectFolder), self.__GetFilelistFilename(_project))
+        csnUtility.ReplaceDestinationFileIfDifferent(self.__GetFilelistFilename(_project), self.__GetFilelistFilename(_project, kdevelopProjectFolder))
         
         f = open(self.__GetKDevelopProjectFilename(_project), 'r')
         kdevelopProjectText = f.read()
