2f4d152802e266b32ed6679e690f011c5f4b1c73
ymartelli <ymartelli@9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf>
2008-12-19 13:14:22 +0000

    Expect a context file as input.
    git-svn-id: https://svncistib.upf.edu/repos/csnake/trunk@290 9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf

--- src/build.py
+++ src/build.py
@@ -8,14 +8,9 @@ import sys
 import csnGUIHandler
 
 # Check command line arguments
-if not len(sys.argv) in (2,3):
-    sys.exit("Error: not enough arguments. You need to provide a context file and (optionally) an options file.")
+if len(sys.argv) != 1:
+    sys.exit("Error: not enough arguments. You need to provide a context file.")
 
-if len(sys.argv) == 3:
-    print "Converting %s using options file %s\n" % (sys.argv[1], sys.argv[2])
-    converter = csnContextConverter(sys.argv[2])
-    converter.Convert(sys.argv[1])
-    
 # Create GUI handler
 handler = csnGUIHandler.Handler()
 context = handler.LoadContext(sys.argv[1])
