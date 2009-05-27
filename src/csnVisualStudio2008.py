f1aa042c6c44e7bd0925ee6fdc228ce11d1264a5
jlyko <jlyko@9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf>
2009-05-27 08:59:44 +0000

    add pecompiled header to the project
    git-svn-id: https://svncistib.upf.edu/repos/csnake/trunk@383 9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf

--- src/csnVisualStudio2008.py
+++ src/csnVisualStudio2008.py
@@ -1,5 +1,6 @@
 import csnContext
 import csnProject
+import csnUtility
 import os
 
 class Context(csnContext.Context):
@@ -31,9 +32,91 @@ class PostProcessor:
         """
         Post processes the vcproj file generated for _project.
         """
-        
+        # vc proj to patch
+        vcprojFilename = "%s/%s.vcproj" % (_project.GetBuildFolder(), _project.name)
         if not _project.dependenciesManager.isTopLevel:
             slnFilename = "%s/%s.sln" % (_project.GetBuildFolder(), _project.name)
             if os.path.exists(slnFilename):
                 os.remove(slnFilename)
-        return
+
+         # if there is a vcproj, and we want a precompiled header
+        if _project.compileManager.precompiledHeader != "" and os.path.exists(vcprojFilename):
+          
+          # binary pch file to generate
+          debugPchFilename = "%s/%s.debug.pch" % (_project.GetBuildFolder(), _project.name)
+          releasePchFilename = "%s/%s.release.pch" % (_project.GetBuildFolder(), _project.name)
+          
+          # this is the name of the cpp file that will build the precompiled headers
+          pchCppFilename = "%sPCH.cpp" % (_project.name)
+          
+          # patch the vcproj
+          f = open(vcprojFilename, 'r')
+          vcproj = f.read()
+          f.close()
+          vcprojOrg = vcproj
+           
+          #set precompiled header
+          searchString = "RuntimeTypeInfo=\"TRUE\"\n"
+          replaceString = \
+"""RuntimeTypeInfo="TRUE"
+UsePrecompiledHeader="2"
+PrecompiledHeaderThrough="%s" 
+PrecompiledHeaderFile="%s"
+ """ % (_project.compileManager.precompiledHeader, releasePchFilename)
+          vcproj = vcproj.replace(searchString, replaceString)
+          # in the first occurence of the release pch filename, correct it to the debug pch filename
+          vcproj = vcproj.replace(releasePchFilename, debugPchFilename, 1)
+ 
+          # add pchCpp to the solution
+          searchString = "<Files>\n"
+          replaceString = \
+"""
+    <Files>
+		<Filter
+			Name="PCH Files"
+			Filter="">
+			<File
+				RelativePath="%s">
+				<FileConfiguration
+					Name="Debug|Win32">
+					<Tool
+						Name="VCCLCompilerTool"
+						UsePrecompiledHeader="1"/>
+				</FileConfiguration>
+				<FileConfiguration
+					Name="Release|Win32">
+					<Tool
+						Name="VCCLCompilerTool"
+						UsePrecompiledHeader="1"/>
+				</FileConfiguration>
+			</File>
+		</Filter>
+""" % pchCppFilename             
+          vcproj = vcproj.replace(searchString, replaceString)
+        
+          # force include of the pch header file
+          searchString = "CompileAs=\"2\"\n"
+          replaceString = \
+"""CompileAs="2"
+ForcedIncludeFiles="%s"
+""" % _project.compileManager.precompiledHeader
+          vcproj = vcproj.replace(searchString, replaceString)
+        
+          # create file pchCppFilename
+          precompiledHeaderCppFilename = "%s/%s" % (_project.GetBuildFolder(), pchCppFilename);
+          precompiledHeaderCppFilenameText = \
+"""// Automatically generated file for building the precompiled headers file. DO NOT EDIT
+#include "%s"
+""" % _project.compileManager.precompiledHeader
+          
+ 
+          if( csnUtility.FileToString(precompiledHeaderCppFilename) != precompiledHeaderCppFilenameText ):
+                f = open(precompiledHeaderCppFilename, 'w')
+                f.write(precompiledHeaderCppFilenameText)
+                f.close() 
+                
+          # write patched vcproj
+          f = open(vcprojFilename, 'w')
+          f.write(vcproj)
+          f.close()
+               
