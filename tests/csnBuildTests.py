e3eeae29b70ea81be94be4206c19d73658798f37
ymartelli <ymartelli@9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf>
2010-03-18 04:19:23 +0000

    added test for application generation.
    git-svn-id: https://svncistib.upf.edu/repos/csnake/trunk@454 9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf

--- tests/csnBuildTests.py
+++ tests/csnBuildTests.py
@@ -6,6 +6,7 @@ import csnProject
 import csnGUIHandler
 import shutil
 import subprocess
+import ConfigParser
 
 class csnBuildTests(unittest.TestCase):
 
@@ -26,13 +27,44 @@ class csnBuildTests(unittest.TestCase):
         # clean up
         shutil.rmtree( csnProject.globalCurrentContext.buildFolder )
         
-    def testBuild(self):
-        """ csnBuildTest: test configuring and building the dummy project. """
+    def testDummyExeBuild(self):
+        """ testDummyExeBuild: test configuring and building the DummyExe project. """
+        self.build( "DummyExe", "executable", "Release" )
+        
+    def testDummyLibBuild(self):
+        """ testDummyLibBuild: test configuring and building the DummyLib project. """
+        self.build( "DummyLib", "lib", "Release" )
+
+    def build(self, projectName, type, buildMode):
+        """ test configuring and building the input project. """
+        # names
+        instanceName = projectName.lower()[0] + projectName[1:]
+        contextFileName = "config/csnake_context.txt"
+        contextNewFileName = "config/csnake_context-%s.txt" % projectName
+        sectionName = "CSnake"
+        if type == "executable":
+            exeName = projectName
+        # ok, a bit fishy, I know the application name...
+        elif type == "lib":
+            exeName = projectName + "App_myApp"
+        
+        # modify the csnake context file
+        cf = ConfigParser.ConfigParser()
+        cf.read(contextFileName)
+        # modify the options
+        cf.set(sectionName, "instance", instanceName)
+        csnakefile = cf.get(sectionName, "csnakefile")
+        csnakefile = csnakefile.replace("TestInstance", projectName)
+        cf.set(sectionName, "csnakefile", csnakefile)
+        # save the new context file
+        contextNewFile = open(contextNewFileName, 'w')
+        cf.write(contextNewFile)
+        contextNewFile.close()
         
         # create GUI handler
         handler = csnGUIHandler.Handler()
         # load context
-        context = handler.LoadContext("config/csnake_context.txt")
+        context = handler.LoadContext(contextNewFileName)
         
         # configure the project
         ret = handler.ConfigureProjectToBuildFolder( True )        
@@ -54,21 +86,21 @@ class csnBuildTests(unittest.TestCase):
             cmdString = "%s %s /build %s" % (path, solutionFile, mode )
         elif( context.compilername.find("KDevelop3") != -1 or
               context.compilername.find("Makefile") != -1 ):
-            cmdString = "cd ../bin/executable/%s/DummyExe; make -s" % mainMode
+            cmdString = "cd ../bin/executable/%s/%s; make -s" % projectName, mainMode
         
         # run compiler    
         ret = subprocess.call(cmdString, shell=True)
-        assert ret == 0, "Compiler returned with an error message."
+        assert ret == 0, "The compiler returned with an error message."
 
         # check the built executable
-        exeFilename = "%s/bin/%s/DummyExe" % (context.buildFolder, mainMode)
+        exeFilename = "%s/bin/%s/%s" % (context.buildFolder, mainMode, exeName)
         if( context.compilername.find("Visual Studio") != -1 ):
             exeFilename = "%s.exe" % (exeFilename)
         assert os.path.exists(exeFilename)
         
         # test the built executable
         ret = subprocess.call(exeFilename, shell=True)
-        assert ret == 6, "DummyExe did not return the correct result."
+        assert ret == 6, "The generated executable did not return the correct result."
 
         # clean up
         shutil.rmtree( csnProject.globalCurrentContext.buildFolder )
