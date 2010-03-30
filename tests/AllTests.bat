d3ab2e217b2a272793c331f02a0f67d35a839cbd
ymartelli <ymartelli@9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf>
2010-03-30 03:40:28 +0000

    Added documentation for test run.
    git-svn-id: https://svncistib.upf.edu/repos/csnake/trunk@479 9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf

--- tests/AllTests.bat
+++ tests/AllTests.bat
@@ -3,6 +3,16 @@ REM --------------------------
 REM Run the csnake tests
 REM --------------------------
 
+REM Theses test depend on a context file specific to your machine.
+REM They expect it to be: ./config/csnake_context.txt
+REM You can use the template one of this folder and replace with your settings.
+REM Important options:
+REM  rootfolder0 = %PATH_TO_TEST_SRC%
+REM  csnakefile = %rootfolder0%/TestInstance/csnTestInstance.py
+REM  instance = testInstance
+REM  thirdpartyrootfolder = %rootfolder0%/src
+REM Build and install folders are created and deleted at the end of the tests
+
 REM Add csnake src to the python path
 @set PYTHONPATH=../src;./src
 REM Run all tests
--- tests/AllTests.sh
+++ tests/AllTests.sh
@@ -3,6 +3,16 @@
 # Run the csnake tests
 # --------------------------
 
+# Theses test depend on a context file specific to your machine.
+# They expect it to be: ./config/csnake_context.txt
+# You can use the template one of this folder and replace with your settings.
+# Important options:
+#  rootfolder0 = %PATH_TO_TEST_SRC%
+#  csnakefile = %rootfolder0%/TestInstance/csnTestInstance.py
+#  instance = testInstance
+#  thirdpartyrootfolder = %rootfolder0%/src
+# Build and install folders are created and deleted at the end of the tests
+
 # Add csnake src to the python path
 export PYTHONPATH=$PYTHONPATH:../src:./src
 # Run all tests
