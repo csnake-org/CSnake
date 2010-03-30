5ff8e321f80faa2674752c901c6cd45a48aeb65c
ymartelli <ymartelli@9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf>
2010-03-30 03:30:26 +0000

    Added test for precompiled header support.
    git-svn-id: https://svncistib.upf.edu/repos/csnake/trunk@478 9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf

--- tests/src/DummyLib/csnDummyLib.py
+++ tests/src/DummyLib/csnDummyLib.py
@@ -6,3 +6,5 @@ dummyLib = csnCilab.CilabModuleProject("DummyLib", "library")
 dummyLib.AddLibraryModules(["dummyLib"])
 dummyLib.AddApplications(["myApp"])
 dummyLib.AddTests(["tests/DummyTest/*.h"], cxxTest)
+# creates a dependency on thirdParty/cmakeMacros/PCHSupport_26.cmake
+dummyLib.SetPrecompiledHeader("dummyLibPCH.h")
