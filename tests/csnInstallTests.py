fb8101f79c1521ebe7df2822fc2a8185b7a16130
ymartelli <ymartelli@9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf>
2010-06-29 09:18:54 +0000

    Added tests for install files.
    git-svn-id: https://svncistib.upf.edu/repos/csnake/trunk@556 9ffc3505-93cb-cd4b-9e5d-8a77f6415fcf

---
+++ tests/csnInstallTests.py
@@ -0,0 +1,41 @@
+# Unit tests for the csnInstall class
+import unittest
+import csnContext
+import csnProject
+import shutil
+import os.path
+import csnUtility
+import logging.config
+
+class csnInstallTests(unittest.TestCase):
+    
+    def setUp(self):
+        """ Run before test. """
+        # load fake context
+        self.context = csnContext.Load("config/csnake_context.txt")
+        # change the build folder
+        self.context.buildFolder = self.context.buildFolder + "/bin"
+        # set it as global context
+        csnProject.globalCurrentContext = self.context
+        # logging init
+        logging.config.fileConfig(csnUtility.GetRootOfCSnake() + "/resources/logging.conf")
+
+    def tearDown(self):
+        """ Run after test. """
+        csnProject.globalCurrentContext = None
+
+    def testInstall(self):
+        """ Test the installation of files. """
+        # dummyLib project
+        dummyInstall = csnProject.Project("DummyInstall", "library")
+        dummyInstall.AddFilesToInstall(["AllTests.bat"], _debugOnly = 1, _WIN32 = 1)
+        dummyInstall.AddFilesToInstall(["AllTests.bat"], _releaseOnly = 1, _WIN32 = 1)
+        dummyInstall.installManager.InstallBinariesToBuildFolder()
+        # check presence of the files
+        self.assertTrue( os.path.exists("bin/bin/Debug/AllTests.bat") )
+        self.assertTrue( os.path.exists("bin/bin/Release/AllTests.bat") )
+        # clean up
+        shutil.rmtree( csnProject.globalCurrentContext.buildFolder )
+
+if __name__ == "__main__":
+    unittest.main()
