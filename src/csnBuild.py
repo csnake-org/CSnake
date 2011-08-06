5cbda2162dec5da2f5485a086b13c381c7e0c12e
msteghofer <msteghofer@cistib.upf.edu>
2011-08-07 01:13:00 +0200

    The version number gets its own class
    Issue #42

--- src/csnBuild.py
+++ src/csnBuild.py
@@ -1,14 +1,14 @@
 ## @package csnBuild
 # Prevent warnings.
 from csnProject import Project, globalCurrentContext
-from csnGenerator import Generator, version, versionArray, versionString, ToProject
+from csnGenerator import Generator, version, versionObject, versionString, ToProject
 
 def PreventWarnings():
     Project
     globalCurrentContext
     Generator
     version         # still there for compatibility and has the same value as versionString
-    versionArray
+    versionObject
     versionString
     ToProject
     
--- src/csnGenerator.py
+++ src/csnGenerator.py
@@ -78,33 +78,70 @@ import re
 # - Replace string labels with class types
 # End: ToDo.
 
-versionModifierValue = { "alpha" : -10000,
-                     "beta" : -5000 }
-
 # add root of csnake to the system path
 sys.path.append(csnUtility.GetRootOfCSnake())
+
+class Version:
+    versionModifierValue = { "alpha"  : -10000,
+                             "alpha1" : -15000,
+                             "alpha2" : -14999,
+                             "alpha3" : -14998,
+                             "alpha4" : -14997,
+                             "alpha5" : -14996,
+                             "beta"   : -10000,
+                             "beta1"  : -10000,
+                             "beta2"  : -9999,
+                             "beta3"  : -9998,
+                             "beta4"  : -9997,
+                             "beta5"  : -9996,
+                             "rc1"    : -5000,
+                             "rc2"    : -4999,
+                             "rc3"    : -4998,
+                             "rc4"    : -4997,
+                             "rc5"    : -4996,
+                             ""       : 0,
+                             None     : 0 }
+
+    def __init__(self, versionString):
+        self.__GetVersionFromString(versionString)
+    
+    def __GetVersionFromString(self, versionString):
+        # divide "1.2.3beta", "1.2.3.beta", "1.2.3 beta" etc. in "1.2.3" and "beta"
+        m = re.match('([0-9]+(\\.[0-9]+)*)[^a-zA-Z0-9]*(.*)', versionString)
+        pureVersionString = m.group(1)
+        self.__versionModifier = m.group(3) # alpha, beta, rc1, etc.
+        
+        # convert version string into an array of ints
+        self.__versionNumber = map(int, pureVersionString.split('.'))
+        
+        # normalize it
+        while len(self.__versionNumber) > 0 and self.__versionNumber[-1] == 0:
+            self.__versionNumber.pop()
+    
+    def GetString(self, numDecimals = 2):
+        zeroes = [0 for i in range(0, numDecimals + 1 - len(self.__versionNumber))]
+        return "%s-%s" % (".".join(map(str, self.__versionNumber + zeroes)), self.__versionModifier)
+    
+    def __cmp__(self, other):
+        commonLength = min(len(self.__versionNumber), len(other.__versionNumber))
+        for i in range(0, commonLength):
+            if self.__versionNumber[i] != other.__versionNumber[i]:
+                return self.__versionNumber[i] - other.__versionNumber[i]
+        if len(self.__versionNumber) == len(other.__versionNumber):
+            return Version.versionModifierValue[self.__versionModifier] - Version.versionModifierValue[other.__versionModifier]
+        elif len(self.__versionNumber) > len(other.__versionNumber):
+            return 1
+        else:
+            return -1
+                    
+
 def getVersion():
     about = About()
     about.read(csnUtility.GetRootOfCSnake() + "/resources/about.txt")
     return about.getVersion()
 
-def getVersionArray(versionString):
-    # divide "1.2.3beta", "1.2.3.beta", "1.2.3 beta" etc. in "1.2.3" and "beta"
-    m = re.match('([0-9](\\.[0-9]+)*)[^a-zA-Z0-9]*(.*)', versionString)
-    pureVersionString = m.group(1)
-    versionModifier = m.group(3) # alpha, beta, rc1, etc.
-    
-    # convert version string into an array of ints
-    result = [int(number) for number in pureVersionString.split('.')]
-    
-    # include pre-release modifier in version number
-    if versionModifier in versionModifierValue:
-        result.append(versionModifierValue[versionModifier])
-    print result
-    return result
-
 versionString = getVersion()
-versionArray = getVersionArray(versionString)
+versionObject = Version(versionString)
 
 # for compatibility still set the old variable (to be removed at some point)
 version = versionString
