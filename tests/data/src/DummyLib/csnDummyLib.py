d926044b9a693ecf4807a3b46805f44110cc18f6
ivmartel <ymartelli@cistib.upf.edu>
2011-09-23 11:19:58 +0200

    Using glob.

--- tests/data/src/DummyLib/csnDummyLib.py
+++ tests/data/src/DummyLib/csnDummyLib.py
@@ -2,6 +2,7 @@
 import csnBuild
 import csnCilab
 from csnAll import *
+import os.path
 
 # define project
 dummyLib = csnCilab.CilabModuleProject("DummyLib", "library")
@@ -15,6 +16,9 @@ dummyLib.AddApplications(["myApp"])
 # tests
 dummyLib.AddTests(["tests/DummyTest/*.h"], cxxTest)
 # creates a dependency on thirdParty/cmakeMacros/PCHSupport_26.cmake
-dummyLib.SetPrecompiledHeader("dummyLibPCH.h")
+found = dummyLib.Glob("*PCH.h")
+pchFile = os.path.basename(found[0])
+dummyLib.SetPrecompiledHeader(pchFile)
 # add compiler definitions
 dummyLib.AddDefinitions(["-Wall -Werror"], _private = 1, _WIN32 = 1, _NOT_WIN32 = 1)
+# search for some files
--- tests/data/src_api/DummyLib/csnDummyLib.py
+++ tests/data/src_api/DummyLib/csnDummyLib.py
@@ -1,6 +1,7 @@
 # Used to configure dummyLib
 from csnAll import *
 from csnAPIPublic import GetAPI
+import os.path
 api = GetAPI("2.4.5")
 
 # define project
@@ -15,6 +16,8 @@ dummyLib.AddApplications(["myApp"])
 # tests
 dummyLib.AddTests(["tests/DummyTest/*.h"], cxxTest)
 # creates a dependency on thirdParty/cmakeMacros/PCHSupport_26.cmake
-dummyLib.SetPrecompiledHeader("dummyLibPCH.h")
+found = dummyLib.Glob("*PCH.h")
+pchFile = os.path.basename(found[0])
+dummyLib.SetPrecompiledHeader(pchFile)
 # add compiler definitions
 dummyLib.AddDefinitions(["-Wall -Werror"], private = 1, WIN32 = 1, NOT_WIN32 = 1)
