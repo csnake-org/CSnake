16e692dfc15f1db9226d1dff5a1f1764c5319d83
ivmartel <ymartelli@cistib.upf.edu>
2011-10-04 16:37:56 +0200

    Uncommented tests.

--- tests/csnBuildDummyLibTests.py
+++ tests/csnBuildDummyLibTests.py
@@ -14,13 +14,13 @@ class csnBuildDummyLibTests(unittest.TestCase):
     def tearDown(self):
         """ Run after test. """
 
-#    def testDummyLibBuild(self):
-#        """ testDummyLibBuild: test configuring and building the DummyLib project. """
-#        config = TestProjectConfig("DummyLib", "lib", "Release", 
-#                                   ["src"], "build",
-#                                   ["thirdParty"], ["build/thirdParty"],
-#                                   "src/DummyLib/csnDummyLib.py")
-#        BuildTest.testBuild( config )
+    def testDummyLibBuild(self):
+        """ testDummyLibBuild: test configuring and building the DummyLib project. """
+        config = TestProjectConfig("DummyLib", "lib", "Release", 
+                                   ["src"], "build",
+                                   ["thirdParty"], ["build/thirdParty"],
+                                   "src/DummyLib/csnDummyLib.py")
+        BuildTest.testBuild( config )
 
     def testDummyLibBuildWithAPI(self):
         """ testDummyLibBuild: test configuring and building the DummyLib project. """
@@ -30,39 +30,39 @@ class csnBuildDummyLibTests(unittest.TestCase):
                                    "src_api/DummyLib/csnDummyLib.py")
         BuildTest.testBuild( config )
 
-#    def testDummyLibBuildMixed(self):
-#        """ testDummyLibBuild: test configuring and building the DummyLib project. """
-#        config = TestProjectConfig("DummyLib", "lib", "Release", 
-#                                   ["src_api"], "build",
-#                                   ["thirdParty"], ["build/thirdParty"],
-#                                   "src_api/DummyLib/csnDummyLib.py")
-#        BuildTest.testBuild( config )
+    def testDummyLibBuildMixed(self):
+        """ testDummyLibBuild: test configuring and building the DummyLib project. """
+        config = TestProjectConfig("DummyLib", "lib", "Release", 
+                                   ["src_api"], "build",
+                                   ["thirdParty"], ["build/thirdParty"],
+                                   "src_api/DummyLib/csnDummyLib.py")
+        BuildTest.testBuild( config )
 
-#    def testDummyLibBuildMixed2(self):
-#        """ testDummyLibBuild: test configuring and building the DummyLib project. """
-#        config = TestProjectConfig("DummyLib", "lib", "Release", 
-#                                   ["src"], "build",
-#                                   ["thirdParty_api"], ["build/thirdParty"],
-#                                   "src/DummyLib/csnDummyLib.py")
-#        BuildTest.testBuild( config )
+    def testDummyLibBuildMixed2(self):
+        """ testDummyLibBuild: test configuring and building the DummyLib project. """
+        config = TestProjectConfig("DummyLib", "lib", "Release", 
+                                   ["src"], "build",
+                                   ["thirdParty_api"], ["build/thirdParty"],
+                                   "src/DummyLib/csnDummyLib.py")
+        BuildTest.testBuild( config )
 
-#    def testDummyLibBuildMultiple(self):
-#        """ testDummyLibBuildMul: test configuring and building the DummyLib project. """
-#        config = TestProjectConfig("DummyLib2", "lib", "Release", 
-#                                   ["src", "src2"], "build",
-#                                   ["thirdParty", "thirdParty2"], ["build/thirdParty", "build/thirdParty2"],
-#                                   "src2/DummyLib2/csnDummyLib2.py")
-#        BuildTest.testBuild( config )
+    def testDummyLibBuildMultiple(self):
+        """ testDummyLibBuildMul: test configuring and building the DummyLib project. """
+        config = TestProjectConfig("DummyLib2", "lib", "Release", 
+                                   ["src", "src2"], "build",
+                                   ["thirdParty", "thirdParty2"], ["build/thirdParty", "build/thirdParty2"],
+                                   "src2/DummyLib2/csnDummyLib2.py")
+        BuildTest.testBuild( config )
 
-#    def testDummyLibBuildWithSpace(self):
-#        """ testDummyLibBuildWithSpace: test configuring and building the DummyLib project with spaces in folders. """
-#        config = TestProjectConfig("DummyLib", "lib", "Release", 
-#                                   ["my src"], "my build",
-#                                   ["third party"], ["my build/third party"],
-#                                   "my src/DummyLib/csnDummyLib.py")
-#        # Only for visual studio
-#        if( config.isVisualStudioConfig() == True ):
-#            BuildTest.testBuild( config )
+    def testDummyLibBuildWithSpace(self):
+        """ testDummyLibBuildWithSpace: test configuring and building the DummyLib project with spaces in folders. """
+        config = TestProjectConfig("DummyLib", "lib", "Release", 
+                                   ["my src"], "my build",
+                                   ["third party"], ["my build/third party"],
+                                   "my src/DummyLib/csnDummyLib.py")
+        # Only for visual studio
+        if( config.isVisualStudioConfig() == True ):
+            BuildTest.testBuild( config )
         
 if __name__ == "__main__":
     unittest.main() 
