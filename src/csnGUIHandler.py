22628e0eb63b6120fff07d67b89363a09ce5d406
ivmartel <ymartelli@cistib.upf.edu>
2011-12-02 18:03:31 +0100

    More project reload! (see #72)

--- src/csnGUIHandler.py
+++ src/csnGUIHandler.py
@@ -145,8 +145,9 @@ class Handler:
     def __GetProjectInstance(self, _forceReload = False):
         """ Instantiates and returns the _instance in _projectPath. """
         instanceName = self.context.GetInstance()
+        reloadFiles = _forceReload or self.__CheckReloadChanges(self.cachedProjectInstanceContext, self.context.GetData())
         
-        if not instanceName in self.cachedProjectInstance:
+        if not instanceName in self.cachedProjectInstance or reloadFiles:
             projectModule = self.__GetProjectModule(_forceReload = _forceReload)
             exec "self.cachedProjectInstance[instanceName] = csnProject.ToProject(projectModule.%s)" % instanceName
             if isinstance(self.cachedProjectInstance[instanceName], csnAPIImplementation._APIGenericProject_Base):
