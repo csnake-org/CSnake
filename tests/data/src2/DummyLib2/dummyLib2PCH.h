a8d5e12a6526ad54b372b022d7fa3b33d17b8b51
ivmartel <ivmartel@gmail.com>
2015-08-05 16:21:15 +0200

    Updated copyright, authors and version.

--- tests/data/src2/DummyLib2/dummyLib2PCH.h
+++ tests/data/src2/DummyLib2/dummyLib2PCH.h
@@ -1,9 +1,3 @@
-/*
-* Copyright (c) 2009,
-* Computational Image and Simulation Technologies in Biomedicine (CISTIB),
-* Universitat Pompeu Fabra (UPF), Barcelona, Spain. All rights reserved.
-* See license.txt file for details.
-*/
 #ifndef DUMMYLIBPCH_h
 #define DUMMYLIBPCH_h
 
